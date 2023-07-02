7bcd589fb186da1f89b09a85497410e58fdb6e11
j3tj3rk <jclea1130@gmail.com>
2023-07-02 15:07:06 -0400

    Update pizza.py
    Turned the main program back on. Linking this as a project on my personal site to force myself to get this program working.

--- pizza.py
+++ pizza.py
@@ -15,29 +15,20 @@ elif zahornah == "nah":
 else:
 	print("Please answer zah or nah, brah!")
 
+Downtown = ['Pizzeria Di Giovanni', 'Renzo', 'Melfis', 'DAllesandro', 'Frannie and the Fox', 'Monza',  'Benny Ravellos', 'Sabatinos  Pizza', 'Baker and Brewer', 'Uneeda Sicilian', 'Renzo', 'Baked Pizzeria']
+print(random.choice(Downtown))
 
+WestAshley = ['Slice', 'Marios',  'Baronis', 'Andolinis']
+print(random.choice(WestAshley))
 
+NCharleston = ['Evo Pizzeria', 'Park Pizza Co']
+print(random.choice(NCharleston))
 
+JamesIsland = ['Famiularis Pizzeria', 'Crust Wood Fired Pizza', 'Dough Boy', 'Paisanos Pizza Grill']
+print(random.choice(JamesIsland))
 
+MtPleasant =  ['La  Pizzeria', 'The Obstinate Daughter']
+print(random.choice(MtPleasant))
 
-
-#Downtown = ['Pizzeria Di Giovanni', 'Renzo', 'Melfis', 'DAllesandro', 'Frannie and the Fox', 'Monza',  'Benny Ravellos', 'Sabatinos  Pizza', 'Baker and Brewer', 'Uneeda Sicilian', 'Renzo', 'Baked Pizzeria']
-#print(random.choice(Downtown))
-
-#WestAshley = ['Slice', 'Marios',  'Baronis', 'Andolinis']
-#print(random.choice(WestAshley))
-
-
-#NCharleston = ['Evo Pizzeria', 'Park Pizza Co']
-#print(random.choice(NCharleston))
-
-#JamesIsland = ['Famiularis Pizzeria', 'Crust Wood Fired Pizza', 'Dough Boy', 'Paisanos Pizza Grill']
-#print(random.choice(JamesIsland))
-
-#MtPleasant =  ['La  Pizzeria', 'The Obstinate Daughter']
-#print(random.choice(MtPleasant))
-
-#DanielIsland = ['Vespa  Pizzeria', 'Orlandos  Pizza']
-#print(random.choice(DanielIsland))
-
-
+DanielIsland = ['Vespa  Pizzeria', 'Orlandos  Pizza']
+print(random.choice(DanielIsland))
